4f6d60b514a5c7289ca1057200b7c0ad6c322401
alainlompo <alain.lompo@gmail.com>
2015-11-26 01:51:47 +0100

    Adding more grouping features (counting, max, ...)

--- java8-labs-home/java8-lambdas/src/main/java/org/lompo/labs/java8/lambdas/streams/grouping/GroupingDemo.java
+++ java8-labs-home/java8-lambdas/src/main/java/org/lompo/labs/java8/lambdas/streams/grouping/GroupingDemo.java
@@ -1,16 +1,61 @@
 package org.lompo.labs.java8.lambdas.streams.grouping;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
+import static java.util.stream.Collectors.maxBy;
+import static java.util.stream.Collectors.counting;
 
 import org.lompo.labs.java8.lambdas.streams.reducing.Transaction;
 import org.lompo.labs.java8.lambdas.streams.reducing.TransactionUtils;
 
-import static java.util.stream.Collectors.*;
+import static java.util.Comparator.comparingDouble;
+import static java.util.stream.Collectors.groupingBy;
+
 
 public class GroupingDemo {
 	
 	public static enum TransactionType { GOLD, SILVER, BRONZE};
 	
+	public static void groupMaxAmountTransactionsByType(List<Transaction> source) {
+		Map<TransactionType, Optional<Transaction>> maxAmountByType 
+		= source.stream()
+		.collect(
+				groupingBy(
+						tx -> {
+							if (tx.getAmount() < 500) return TransactionType.BRONZE;
+							else if (tx.getAmount() < 1000) return TransactionType.SILVER;
+							return TransactionType.GOLD;
+						},
+						maxBy(comparingDouble(Transaction::getAmount))
+						)
+				
+				);
+		for (TransactionType tT: maxAmountByType.keySet()) {
+			System.out.println("Type: " + tT);
+			System.out.println("====================");
+			System.out.println(maxAmountByType.get(tT));
+		}
+	}
+	
+	public static void groupTransactionsByTypeAndCounting(List<Transaction> source) {
+		Map<TransactionType, Long>
+		trxByType = source.stream()
+		.collect(
+				groupingBy(
+						tx -> {
+							if (tx.getAmount() < 500) return TransactionType.BRONZE;
+							else if (tx.getAmount() < 1000) return TransactionType.SILVER;
+							return TransactionType.GOLD;
+						}, counting()
+						)
+				);
+		
+		for (TransactionType tT: trxByType.keySet()) {
+			System.out.println("Type: " + tT);
+			System.out.println("====================");
+			System.out.println(trxByType.get(tT));
+		}
+	}
 	public static void groupTransactionsByType (List<Transaction> source) {
 		Map<TransactionType, List<Transaction>>
 		trxByType = source.stream()
@@ -49,6 +94,12 @@ public class GroupingDemo {
 		System.out.println();
 		System.out.println("Now classifying transactions according to their significance...");
 		groupTransactionsByType(transactions);
+		System.out.println();
+		System.out.println("Now group and count....");
+		groupTransactionsByTypeAndCounting(transactions);
+		System.out.println();
+		System.out.println("Now group by type and display, for each group the trx with max amount...");
+		groupMaxAmountTransactionsByType(transactions);
 	}
 
 }
