a6a25942576ba1819a52fbd4e8979b95713216bd
alainlompo <alain.lompo@gmail.com>
2016-02-03 02:09:37 +0100

    enriching the LocalDate example

--- java8-labs-home/java8-lambdas/src/main/java/org/lompo/labs/java8/lambdas/dateAndTime/DateDemo1.java
+++ java8-labs-home/java8-lambdas/src/main/java/org/lompo/labs/java8/lambdas/dateAndTime/DateDemo1.java
@@ -6,11 +6,22 @@ public class DateDemo1 {
 	public static void main(String[] args) {
 		LocalDate date = LocalDate.of(2014,3,18);
 		System.out.println(String.format(
-				"Year: %d, month: %s, and day: %s",
+				"Year: %d, month: %s, and day: %s, the  %s th",
 				date.getYear(),
 				date.getMonth().toString(),
+				date.getDayOfWeek(),
 				date.getDayOfMonth()
 				));
+		
+		System.out.println(String.format("The year %d is ", date.getYear())
+				+ ((date.isLeapYear())?"":"not") + " a leap year"
+				);
+		
+		LocalDate rightNow = LocalDate.now();
+		System.out.println("Today is " + rightNow.getDayOfWeek() + " the " + rightNow.getDayOfMonth() + " th of " + rightNow.getMonth().toString() + " "  +  rightNow.getYear());
+		
+		
+		
 	}
 
 }
