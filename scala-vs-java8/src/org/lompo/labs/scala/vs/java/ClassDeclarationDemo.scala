21698a84223d386457ed83fa1cf22d6795c0e418
alainlompo <alain.lompo@gmail.com>
2016-02-20 13:18:09 +0100

    Basic Scala class declaration and usage

---
+++ scala-vs-java8/src/org/lompo/labs/scala/vs/java/ClassDeclarationDemo.scala
@@ -0,0 +1,10 @@
+package org.lompo.labs.scala.vs.java
+
+/**
+ * @author LOMPO
+ */
+class ClassDeclarationDemo {
+  def greetSomeone(someOne: String) {
+    println("greetings " + someOne)
+  }
+}
---
+++ scala-vs-java8/src/org/lompo/labs/scala/vs/java/Greeter.scala
@@ -0,0 +1,11 @@
+package org.lompo.labs.scala.vs.java
+
+/**
+ * @author LOMPO
+ */
+object Greeter {
+   def main(args: Array[String]) {
+    val greeter = new ClassDeclarationDemo()
+    greeter.greetSomeone("John Parker")
+  }
+}
