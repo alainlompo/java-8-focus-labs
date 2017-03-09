2553f28cf8a1533d49cacccc2aacf5bb8a12e111
Alain Lompo <alain.lompo@gmail.com>
2017-03-09 17:55:13 +0100

    Update PolarMoveable.java

--- java8-labs-home/java8-lambdas/src/main/java/org/lompo/labs/java8/interfaces/defaultmethods/PolarMoveable.java
+++ java8-labs-home/java8-lambdas/src/main/java/org/lompo/labs/java8/interfaces/defaultmethods/PolarMoveable.java
@@ -1,5 +1,9 @@
 package org.lompo.labs.java8.interfaces.defaultmethods;
 
+// Any class that implements this interface
+// will automatically benefit from these default methods without needing to redefine them
+// This way some kind of ascending compatibility can be defined. So the new methods introduced by java 8 and its functional model
+// do not break former apis from former java version. Similar to C# extension methods
 public interface PolarMoveable {
 	
 	double getR();
